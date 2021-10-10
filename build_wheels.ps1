ae7a73595dddde5c7ff7e9da3e34c28c834d9a90
Joachim Folz <joachim.folz@gmail.com>
2021-10-10 14:16:13 +0000

    different installdir

--- build_wheels.ps1
+++ build_wheels.ps1
@@ -11,25 +11,27 @@ Echo $Env:PYVERS
 Echo "Choco args:"
 Echo $Env:CHOCOARGS
 
+$pydir = "C:\chocopython"
+
 # Compile & test wheels
 foreach ($pyver in $pyvers){
     # install
-    & choco install python3 -y -r --force --no-progress --version=$pyver $chocoargs --params "/InstallDir:C:\python"
+    & choco install python3 -y -r --force --no-progress --version=$pyver $chocoargs --params "/InstallDir:$pydir"
     if ($LASTEXITCODE -ne 0) { throw "build failed with exit code $LASTEXITCODE" }
-    & C:\python\python.exe -m pip install -U pip --no-warn-script-location
+    & $pydir\python.exe -m pip install -U pip --no-warn-script-location
     if ($LASTEXITCODE -ne 0) { throw "build failed with exit code $LASTEXITCODE" }
-    & C:\python\python.exe -m pip install -q build
+    & $pydir\python.exe -m pip install -q build
     # build
     if ($LASTEXITCODE -ne 0) { throw "build failed with exit code $LASTEXITCODE" }
-    & C:\python\python.exe -m build --wheel  --outdir dist
+    & $pydir\python.exe -m build --wheel  --outdir dist
     if ($LASTEXITCODE -ne 0) { throw "build failed with exit code $LASTEXITCODE" }
     # test
     cd test
-    & C:\python\python.exe -m pip install --only-binary ":all:" -r ..\test_requirements.txt --no-warn-script-location
+    & $pydir\python.exe -m pip install --only-binary ":all:" -r ..\test_requirements.txt --no-warn-script-location
     if ($LASTEXITCODE -ne 0) { throw "build failed with exit code $LASTEXITCODE" }
-    & C:\python\python.exe -m pip install simplebloom --no-index -f ..\dist
+    & $pydir\python.exe -m pip install simplebloom --no-index -f ..\dist
     if ($LASTEXITCODE -ne 0) { throw "build failed with exit code $LASTEXITCODE" }
-    & C:\python\python.exe -m pytest -vv
+    & $pydir\python.exe -m pytest -vv
     if ($LASTEXITCODE -ne 0) { throw "test failed with exit code $LASTEXITCODE" }
     cd ..
     & choco uninstall python3 -y -r --no-progress
