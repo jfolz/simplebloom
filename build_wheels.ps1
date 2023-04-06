c8cb695ff3bc17a3cec24eadfd7e3ac1fc9a249e
Joachim Folz <joachim.folz@gmail.com>
2023-04-06 13:02:10 +0000

    use appveyor for windows

--- build_wheels.ps1
+++ build_wheels.ps1
@@ -3,38 +3,28 @@ $ErrorActionPreference = "Continue";
 
 # the list of Python interpreters
 $pyvers = $Env:PYVERS -split ";"
-$chocoargs = $Env:CHOCOARGS -split " "
 
 Echo "Selected Python versions:"
 Echo $Env:PYVERS
 
-Echo "Choco args:"
-Echo $Env:CHOCOARGS
-
-$pydir = "C:\chocopython"
-
 # Compile & test wheels
-foreach ($pyver in $pyvers){
+foreach ($python in $pyvers){
     # install
-    & choco install python3 -y -r --force --no-progress --version=$pyver $chocoargs --params "/InstallDir:$pydir"
     if ($LASTEXITCODE -ne 0) { throw "build failed with exit code $LASTEXITCODE" }
-    & $pydir\python.exe -m pip install -U pip --no-warn-script-location
+    & $python\python.exe -m pip install -U pip --no-warn-script-location
     if ($LASTEXITCODE -ne 0) { throw "build failed with exit code $LASTEXITCODE" }
-    & $pydir\python.exe -m pip install -q build
+    & $python\python.exe -m pip install -q build
     # build
     if ($LASTEXITCODE -ne 0) { throw "build failed with exit code $LASTEXITCODE" }
-    & $pydir\python.exe -m build --wheel  --outdir dist
+    & $python\python.exe -m build --wheel --outdir dist
     if ($LASTEXITCODE -ne 0) { throw "build failed with exit code $LASTEXITCODE" }
     # test
     cd test
-    & $pydir\python.exe -m pip install --only-binary ":all:" -r ..\test_requirements.txt --no-warn-script-location
+    & $python\python.exe -m pip install --only-binary ":all:" -r ..\test_requirements.txt --no-warn-script-location
     if ($LASTEXITCODE -ne 0) { throw "build failed with exit code $LASTEXITCODE" }
-    & $pydir\python.exe -m pip install simplebloom --no-index -f ..\dist
+    & $python\python.exe -m pip install simplebloom --no-index -f ..\dist
     if ($LASTEXITCODE -ne 0) { throw "build failed with exit code $LASTEXITCODE" }
-    & $pydir\python.exe -m pytest -vv
+    & $python\python.exe -m pytest -vv
     if ($LASTEXITCODE -ne 0) { throw "test failed with exit code $LASTEXITCODE" }
     cd ..
-    # cleanup env
-    & choco uninstall python3 -y -r --no-progress
-    Remove-Item -Force -Recurse -Path "$pydir"
 }
--- check_artifacts.sh
+++ check_artifacts.sh
@@ -3,7 +3,7 @@ set -e -x
 
 PYBIN=/opt/python/cp39-cp39/bin
 "${PYBIN}/python" check_artifacts.py dist \
-    --python-versions cp36 cp37 cp38 cp39 cp310 \
+    --python-versions cp37 cp38 cp39 cp310 cp311 \
     --platforms macosx_10_9_x86_64 \
                 manylinux1_i686 \
                 manylinux1_x86_64 \
@@ -27,4 +27,8 @@ PYBIN=/opt/python/cp39-cp39/bin
     --exclude cp310-manylinux1_i686 \
               cp310-manylinux1_x86_64 \
               cp310-manylinux_2_5_i686 \
-              cp310-manylinux_2_5_x86_64
+              cp310-manylinux_2_5_x86_64 \
+              cp311-manylinux1_i686 \
+              cp311-manylinux1_x86_64 \
+              cp311-manylinux_2_5_i686 \
+              cp311-manylinux_2_5_x86_64
